8444a7bd466df2801b603530be661d36d6f1d0bd
medussd <medussd@medussd-PC>
2015-07-03 00:16:53 +0200

    cuentas

--- cuentas/js/controllers.js
+++ cuentas/js/controllers.js
@@ -48,17 +48,22 @@
 
             });             
             servicios.recuperapagadores().success(function(data){
-                data[0].datos.push({ usuario : "Nuevo"});
-                for(i=0;i<data[0].datos.length;i++){
-                    data[0].datos[i].estado="n";
-                }   
-                vs.pagadores=data[0].datos;
+                if(data[0].resultado==0){
+                    $location.path("/login");
+                }else{
+                    data[0].datos.push({ usuario : "Nuevo"});  
+                    vs.pagadores=data[0].datos;  
+                }
+
                 //console.log(vs.pagadores);
 
             });            
             servicios.recuperagastos().success(function(data){
                 for(i=0;i<data.length;i++){
                     data[i].estado="n";
+                    for(a=0;a<data[i].pagadores.length;a++){
+                        data[i].pagadores[a].estado="n";
+                    } 
                 }     
                 vs.gastos=data;
                 vs.seleccionado=vs.gastos[0];
@@ -67,15 +72,17 @@
                 }
             });
             vs.addinterviniente= function(){
-                vs.seleccionado.pagadores.push({"usuario": "Nuevo", gasto: vs.seleccionado.id, id: "", importe_pagado: "0", importe_pagar: "0"});
-                vs.seleccionado.estado='m';
+                vs.seleccionado.pagadores.push({"usuario": "Nuevo", "gasto": vs.seleccionado.id, id: "", "importe_pagado": "0", "importe_pagar" : "0", "estado" : "a"});
+                if(vs.seleccionado.estado!="a"){ 
+                    vs.seleccionado.estado='m';
+                }
                 //vs.modificado=true;
                 //console.log(vs.seleccionado);
             }
             vs.addgasto= function(){
                 grupo={"id":"-","nombre":"-"};
                 pagadores=[];
-                vs.gastos.push({"id":"-","nombre":"-","descripcion":"-","grupo":grupo,"importe":"0","peridiocidad":"1","fecha":"2015-06-27 23:48:04","pagadores": pagadores,"estado":"a"});
+                vs.gastos.push({"id":"","nombre":"-","descripcion":"-","grupo":grupo,"importe":"0","peridiocidad":"0","fecha":"","pagadores": pagadores,"estado":"a"});
                 vs.seleccionado = vs.gastos[vs.gastos.length-1];
                 //vs.modificado=true;
                 //console.log(vs.gastos);
@@ -89,11 +96,30 @@
                     obj.estado="d"; 
                     //vs.modificado=true;
                 }
+                vs.seleccionado.estado='m';
                 //console.log(vs.gastos);
-            }   
+            } 
+            vs.igualar= function(){
+                var total = vs.seleccionado.importe;
+                var impigualado= total/vs.seleccionado.pagadores.length;
+                for(i=0;i<vs.seleccionado.pagadores.length;i++){
+                    vs.seleccionado.pagadores[i].importe_pagar=impigualado.toFixed(2);
+                }
+            }            
             vs.guardar= function(){
                 console.log(vs.gastos);
-            }             
+            }
+            vs.duplicagasto= function(obj){
+                vs.gastos.push({"id":"","nombre": obj.nombre + '_copia',"descripcion":obj.descripcion,"grupo":obj.grupo,"importe":obj.importe,"peridiocidad":obj.peridiocidad,"fecha":"","pagadores": obj.pagadores,"estado":"a"});
+                vs.seleccionado = vs.gastos[vs.gastos.length-1];
+            }            
+            vs.modificagasto= function(obj){
+                if(obj.estado!="a"){
+                    obj.estado="m";
+                    vs.seleccionado.estado='m';
+                }
+                //console.log(vs.gastos);
+            }
             vs.removegasto= function(obj){
                     var existe = 0;
                     var ind = vs.gastos.indexOf(obj);
